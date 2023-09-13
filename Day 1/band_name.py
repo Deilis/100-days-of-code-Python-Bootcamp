9373c9e2924589a8009de15ae875e9237489b943
Deividas Lis <80956337+Deilis@users.noreply.github.com>
2023-09-13 12:23:04 +0300

    Create band_name.py

---
+++ Day 1/band_name.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator for 100 days of Code: The Complete Python Pro Bootcamp for 2023.")
+city = input("What's the name of city you were born?\n")
+pet = input ("What's your first pet name?\n")
+print("Your band name would be " + city + " " + pet)
