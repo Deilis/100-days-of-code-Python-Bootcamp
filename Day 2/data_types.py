e990230e1b77ca4687c2cfe77e7096237d50392c
Deividas Lis <80956337+Deilis@users.noreply.github.com>
2023-09-13 14:09:06 +0300

    Update data_types.py

--- Day 2/data_types.py
+++ Day 2/data_types.py
@@ -1,9 +1,14 @@
 # 🚨 Don't change the code below 👇
+
 two_digit_number = input("Type a two digit number: ")
+
 # 🚨 Don't change the code above 👆
 ####################################
 #Write your code below this line 👇
+
 number_one = two_digit_number[0]
 number_two = two_digit_number[1]
+
 sum = int(number_one) + int(number_two)
+
 print(sum)
